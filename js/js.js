6a1dafb402ef6ce9caf3646458016796f2a8a98f
Ismael Treviño <60386407+Ismola@users.noreply.github.com>
2022-02-23 09:16:53 +0100

    Movimiento de la serpiente conseguido

--- js/js.js
+++ js/js.js
@@ -1,7 +1,9 @@
+// DECLARACIONES DE LAS VARIABLES PARA EL JUEGO
+// --------------------------------------------
 const pantalla = document.getElementById("pantalla");
 const ctx = pantalla.getContext("2d");
 // Cuadricula de la pantalla del juego
-let cuadricula = 5;
+let cuadricula = 30;
 // Altura y a anchura en pixeles de la pantalla
 pantalla.height = 300;
 pantalla.width = 300;
@@ -9,64 +11,102 @@ let altura = pantalla.height;
 let anchura = pantalla.width;
 // Tamaño de cada cuadraro de la cuadricula
 let cuadrado = altura / cuadricula;
-let snake = [
-    [0, 0],
-    [cuadrado * 1, 0],
-    [cuadrado * 2, 0],
-];
+//--------- Creacion de la serpiente --------
+// Estas variables almacenarán el las coordenadas x e y de cada cuadrado que compone el cuerpo de la serpiente
+let x, y, coords;
+let tamañoInicial = 4;
+// La serpiente será un array, que contendrá tantos arrays como cuadrados conformen su cuerrpo. Cada array contendrá dos valores, X e Y
+let snake = Array();
+// Esta variable guardará la ultima tecla que hemos dado
+let movimiento;
+// Esta almacenará el ultimo movimiento de la serpiente. Lo usaremos para que la serpiente no pueda ir para atras y hacer cosas raras
+let ultimoMovimiento;
 
-// Funcion principal
-// setInterval(frame, 1000 / 1);
+
+
+
+crearSerpiente(tamañoInicial);
+// PARTE DE LAS FUNCIONES DEL JUEGO
+// --------------------------------------
+// llamada al bucle principal. Se supone que 1000/15 son 15 fotogramas por segundo
+setInterval(frame, 1000 / 15);
+
+// bucle principal
 function frame() {
     moverSnake();
 }
-
-// Indicar la posicion
-let movimiento;
+// Registrador de movimiento de teclas
 document.addEventListener("keyup", function (e) {
     switch (e.code) {
         case "ArrowUp":
-            if (movimiento != 3) {
+            if (ultimoMovimiento != 3) {
                 movimiento = 1;
             }
             break;
         case "ArrowRight":
-            if (movimiento != 4) {
+            if (ultimoMovimiento != 4) {
                 movimiento = 2;
             }
             break;
         case "ArrowDown":
-            if (movimiento != 1) {
+            if (ultimoMovimiento != 1) {
                 movimiento = 3;
             }
             break;
         case "ArrowLeft":
-            if (movimiento != 2) {
+            if (ultimoMovimiento != 2) {
                 movimiento = 4;
             }
             break;
         default:
             break;
     }
-    frame();
 });
-
-
+// Funcion que crea a la serpiente
+function crearSerpiente(tamañoSerpiente) {
+    x = 0, y = 0, coords = Array();
+    snake = Array();
+    for (let i = 0; i < tamañoSerpiente; i++) {
+        x = i * cuadrado;
+        y = 0;
+        coords = Array(x, y);
+        snake.push(coords)
+    }
+}
 function moverSnake() {
     ctx.clearRect(0, 0, pantalla.height, pantalla.width);
     switch (movimiento) {
         case 1:
-            x =
-            snake.push();
+            ultimoMovimiento = 1;
+            x = ((snake[snake.length - 1][0]));
+            y = ((snake[snake.length - 1][1] - cuadrado));
+            coords = Array(x, y);
+            snake.push(coords);
+            snake.shift();
             break;
         case 2:
-
+            ultimoMovimiento = 2;
+            x = ((snake[snake.length - 1][0]) + cuadrado);
+            y = (snake[snake.length - 1][1]);
+            coords = Array(x, y);
+            snake.push(coords);
+            snake.shift();
             break;
         case 3:
-
+            ultimoMovimiento = 3;
+            x = ((snake[snake.length - 1][0]));
+            y = ((snake[snake.length - 1][1] + cuadrado));
+            coords = Array(x, y);
+            snake.push(coords);
+            snake.shift();
             break;
         case 4:
-
+            ultimoMovimiento = 4;
+            x = ((snake[snake.length - 1][0]) - cuadrado);
+            y = (snake[snake.length - 1][1]);
+            coords = Array(x, y);
+            snake.push(coords);
+            snake.shift();
             break;
         default:
             break;
